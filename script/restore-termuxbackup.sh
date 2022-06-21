20b4d6abdd45091d8e9b96264dc9ecc4925b8d66
fscty <waionshredder@protonmail.com>
2022-06-21 17:51:20 +0100

    update

--- script/restore-termuxbackup.sh
+++ script/restore-termuxbackup.sh
@@ -7,7 +7,7 @@ if [ "$CONT" = "y" ]; then
   echo "Restoring Termux Environnement from /storage/emulated/0/utiles/termuxbackup/termux-backup.tar.gz\nSize of the backup: $SIZE\nLast Backup: \033[1;34m $DATE \033[0m......\n......\n.....\n...It takes a few minutes depending on size of your backup\n"
   tar -zxf /storage/emulated/0/utiles/termuxbackup/termux-backup.tar.gz  -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
   echo "\033[1;34mRESTORATION DONE\033[0m";
-  apt-mark hold openssl postgresql openssl-1.1 openssl-1.1-static openssl-static openssl-tool openssl1.1-tool sslscan
+  apt-mark hold openssl postgresql openssl-1.1 openssl-1.1-static openssl-static openssl-tool openssl1.1-tool sslscan libgcrypt libpcap libxml2   
   echo "\033[1;34mThe packages have been blocked from updates\033[0m";
 else
   echo "exit";
