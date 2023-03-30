fbfe9fcad45ff3c3632b6e27f40c5babed4f15d9
vakandi <wael.bousfira@protonmail.com>
2023-03-30 00:42:30 +0000

    update

--- pico-ducky/setup.sh
+++ pico-ducky/setup.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 userr="vakandi"
+echo "Use this command before to setup the right firmware:\n"
+echo "cp adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2 /media/$userr/RPI-RP2"                                                                                               0:12
+echo "If you already did, type yes"
+read choice
+if [ $choice = "yes" ]
+then
+	cp -r adafruit_hid /media/$userr/CIRCUITPY/lib/
 
-cp adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2 /media/$userr/RPI-RP2                                                                                                   0:12
-sleep 10s
-cp -r adafruit_hid /media/$userr/CIRCUITPY/lib/
-
-cp adafruit_debouncer.mpy /media/$userr/CIRCUITPY/lib/
-cp adafruit_ticks.mpy /media/$userr/CIRCUITPY/lib/
-
-cp -r asyncio /media/$userr/CIRCUITPY/lib
-cp -r adafruit_wsgi /media/$userr/CIRCUITPY/lib
-cp -r boot.py /media/$userr/CIRCUITPY/lib
-cp -r duckyinpython.py /media/$userr/CIRCUITPY/lib
-cp -r code.py /media/$userr/CIRCUITPY/lib
-cp -r webapp.py /media/$userr/CIRCUITPY/lib
-cp -r wsgiserver.py /media/$userr/CIRCUITPY/lib
+	cp adafruit_debouncer.mpy /media/$userr/CIRCUITPY/lib/
+	cp adafruit_ticks.mpy /media/$userr/CIRCUITPY/lib/
+	
+	cp -r asyncio /media/$userr/CIRCUITPY/lib
+	cp -r adafruit_wsgi /media/$userr/CIRCUITPY/lib
+	cp -r boot.py /media/$userr/CIRCUITPY/lib
+	cp -r duckyinpython.py /media/$userr/CIRCUITPY/lib
+	cp -r code.py /media/$userr/CIRCUITPY/lib
+	cp -r webapp.py /media/$userr/CIRCUITPY/lib
+	cp -r wsgiserver.py /media/$userr/CIRCUITPY/lib
+else
+	echo "exiting.."
+	exit
+fi
